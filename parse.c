4185b0719136e126b422d980be0fec99be1ad3cc
Sol Jeong <soljeong@c2r3s7.42seoul.kr>
2024-02-05 20:31:32 +0900

    pushswap

--- parse.c
+++ parse.c
@@ -42,6 +42,26 @@ void	stack_init(t_stack *stack, char **splited){
 	}
 }
 
+void	stack_init_ordinal(t_stack *stack, t_stack_node *current){
+	t_stack_node	*node;
+	int				idx;
+
+	node = stack->first;
+	idx = 0;
+	while(idx < stack->cnt){
+
+//그냥 while문으로 돌면서
+//그 전에 있던것 중 현재 노드보다 큰것은 +1하고,
+// 작은 것은 냅둠.
+// 그중에서 현재노드의 위치를 찾으려면...
+/// 1 3 7 6 2 8 4   일때 5가 들어간다면
+//  1 3 6 5 2 7 4 일것임 여기서 5가 들어간다면
+//  1 3 7 6
+// 이때 6을 저장을 하는데, 여기서 6이되면 다시 5로 저장 (6을 뺏겼으니..)
+// 이런식으로 하면..
+	}
+}
+
 void	is_error(){
 	ft_printf("Error\n");
 	exit(0);
